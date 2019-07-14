cfdc01b887d844466de97229c582b045b983b2f6
Sujiehao <sujiehao@itcast.cn>
2019-07-14 10:50:28 +0800

    小程序生命周期函数

--- pages/classify/classify.js
+++ pages/classify/classify.js
@@ -5,42 +5,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    activeIndex: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log("onLoad--监听页面加载");
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log("onReady--监听页面初次渲染完成");
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log("onShow--监听页面显示");
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log("onHide--监听页面隐藏");
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log("onUnload--监听页面卸载");
   },
 
   /**
