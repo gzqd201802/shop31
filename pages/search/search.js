a4c43e89d3774a45279b618eed8a991b7a07e6ef
Sujiehao <sujiehao@itcast.cn>
2019-07-16 12:42:06 +0800

    小程序本地存储实现搜索历史

--- pages/search/search.js
+++ pages/search/search.js
@@ -5,77 +5,115 @@ Page({
    * 页面的初始数据
    */
   data: {
-    keyword:""
+    keyword: "",
+    // 搜索历史列表
+    historyList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    const { keyword } = options;
+  onLoad: function(options) {
+    const {
+      keyword
+    } = options;
     this.setData({
       keyword
     });
   },
 
-  // 当用户键盘输入时触发
-  inputChange(event){
-    const { value } = event.detail;
-    console.log(value);
+  // bindinput - 当用户键盘输入时触发
+  inputChange(event) {
+    const {
+      value
+    } = event.detail;
+    // console.log(value);
   },
 
-  // 当用户点击键盘右下角搜索按钮的时候触发 
+  // bindconfirm - 当用户点击键盘右下角搜索按钮的时候触发
   inputSumbit(event) {
-    const { value } = event.detail;
-    console.log(value);
+    const {
+      value
+    } = event.detail;
+    // console.log(value);
+    // 1. 先把数据展示到页面中
+    // 1.1 获取页面数据
+    let {
+      historyList
+    } = this.data;
+    // 1.2 搜索的数据前添加数据
+    historyList.unshift(value);
+    // 1.3 数组去重
+    historyList = [...new Set(historyList)]
+    // 1.4 更新数据
+    this.setData({
+      historyList
+    });
+
+    // 2. 把数据更新到本地存储中
+    // 异步写法
+    // wx.setStorage({
+    //   // 键名称需要写成字符串
+    //   key: 'historyList',
+    //   // 数据应该保留格式
+    //   data: historyList
+    // });
+    // 同步写法
+    wx.setStorageSync('historyList', historyList);
+
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 点击叉叉移除搜索历史
+  removeHistory() {
+    // 1. 视图更新
+    this.setData({
+      historyList: []
+    });
+    // 2. 本地存储更新
+    wx.removeStorageSync('historyList');
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function() {
+    this.setData({
+      // 获取本地存储的搜索历史，如果没有历史，设置成空数组 []
+      historyList: wx.getStorageSync('historyList') || []
+    });
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
