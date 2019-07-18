a2ef862322782db5a67cc11e0d675ecc67b29f5d
Sujiehao <sujiehao@itcast.cn>
2019-07-18 13:08:59 +0800

    收货地址基本功能

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -5,62 +5,96 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    address: {
+      userName: '',
+      telNumber: '',
+      addressInfo: ''
+    }
+  },
+  // 调用收货地址功能
+  chooseAddress() {
+    // 把收货地址的功能封装，因为用户可能点击了不授权，需要特殊处理
+    this.chooseAddressMain();
   },
 
+  // 选择收货地址的核心函数
+  chooseAddressMain() {
+    // 微信内部原生的收货地址界面，而且所有小程序收货地址信息都是互通的
+    wx.chooseAddress({
+      success: res => {
+        console.log(res);
+        const {
+          userName,
+          telNumber,
+          provinceName,
+          cityName,
+          countyName,
+          detailInfo
+        } = res;
+        const address = {
+          userName,
+          telNumber,
+          addressInfo: provinceName + cityName + countyName + detailInfo
+        }
+        // 把地址设置到页面中
+        this.setData({
+          address
+        });
+
+        // 保存到本地存储中
+        wx.setStorageSync('address', address);
+      }
+    });
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function() {
+    this.setData({
+      address: wx.getStorageSync('address') || {}
+    })
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
